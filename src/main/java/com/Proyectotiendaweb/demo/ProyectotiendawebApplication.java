902e6f9a43cfab76b524ec0b6a3f99c87de6e441
andieljm <90933472+andieljm@users.noreply.github.com>
2022-06-08 16:05:33 -0600

    Creacion del proyecto

---
+++ src/main/java/com/Proyectotiendaweb/demo/ProyectotiendawebApplication.java
@@ -0,0 +1,13 @@
+package com.Proyectotiendaweb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectotiendawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectotiendawebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyectotiendaweb/demo/ProyectotiendawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyectotiendaweb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectotiendawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
