f9483a79683bfed85b3d73113a041316625bd10a
andieljm <90933472+andieljm@users.noreply.github.com>
2022-07-14 00:35:32 -0600

    avance 1 agregando archivos html

--- src/main/java/com/Proyectotiendaweb/demo/ProyectotiendawebApplication.java
+++ src/main/java/com/Proyectotiendaweb/demo/ProyectotiendawebApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectotiendawebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectotiendawebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectotiendawebApplication.class, args);                    
+    }
 
 }
