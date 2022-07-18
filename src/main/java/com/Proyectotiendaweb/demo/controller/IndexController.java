b85872329809bf971359184e6b1b0eaae7e8a8f8
AndresCanul <98436130+AndresCanul@users.noreply.github.com>
2022-07-18 10:20:36 -0600

    Actualizacion

--- src/main/java/com/Proyectotiendaweb/demo/controller/IndexController.java
+++ src/main/java/com/Proyectotiendaweb/demo/controller/IndexController.java
@@ -1,28 +1,13 @@
-package com.Proyectotiendaweb.demo.controller;
 
-import com.Proyectotiendaweb.demo.service.ProductoService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+package com.Proyectotiendaweb.demo.controller;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
-@Slf4j
 public class IndexController {
-
-    @Autowired
-    //private ClienteDao clienteDao;
-    private ProductoService productoService; // En caso de que se use @Service
-
-    @GetMapping("/")
-    public String inicio(Model model) {
-        log.info("Ahora se usa arquitectura MVC");
-
-        var producto = productoService.getProductos();
-
-        model.addAttribute("producto", producto);
-
+    @RequestMapping("/")
+    public String start(){
         return "index";
     }
 }
