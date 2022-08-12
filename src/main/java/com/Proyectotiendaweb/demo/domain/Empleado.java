e7159c9d0c971de7fa84b14bec4b840a7e80712f
AndresCanul <98436130+AndresCanul@users.noreply.github.com>
2022-08-11 23:53:10 -0600

    Actualizacion

--- src/main/java/com/Proyectotiendaweb/demo/domain/Empleado.java
+++ src/main/java/com/Proyectotiendaweb/demo/domain/Empleado.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.Proyectotiendaweb.demo.domain;
 
 import java.io.Serializable;
 import javax.persistence.*;
 import lombok.Data;
 
-/**
- *
- * @author Warre
- */
 @Data
 @Entity
 @Table (name = "empleado")
